022aa2dbc523640521e309037254ad81d40e3f20
Rems <remi.gazquez@gmail.com>
2020-12-04 20:07:43 +0100

    Added coin_listing from coingecko

--- strategy/urls.py
+++ strategy/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('risk', views.risk, name='risk'),
     path('returns', views.returns, name='returns'),
     path('input', views.input, name='input'),
+    path('coins_list', views.coins_list, name='coins_list'),
 ]
--- strategy/views.py
+++ strategy/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+import requests
 
 def risk(request):
     RISK_MODEL = {
@@ -32,3 +33,11 @@ def input(request):
     print(json.loads(request.body))
     output = {"truc": 123}
     return HttpResponse(output)
+
+
+def coins_list(request):
+    url = "https://api.coingecko.com/api/v3/coins/list"
+    r = requests.get(url)
+
+    a = json.dumps(r.json()[:10])
+    return HttpResponse(a)
